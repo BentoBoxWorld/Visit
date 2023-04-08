ff522ea488d228abb1abcb8b86cb7937e7fb30e5
BONNe <bonne@bonne.id.lv>
2023-04-08 18:25:59 +0300

    Update VisitPladdon.java

--- src/main/java/world/bentobox/visit/VisitPladdon.java
+++ src/main/java/world/bentobox/visit/VisitPladdon.java
@@ -7,9 +7,6 @@
 package world.bentobox.visit;
 
 
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
@@ -19,8 +16,6 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  * This is a dummy class that allows to load addon via Bukkit Plugin loader.
  * @author BONNe
  */
-@Plugin(name="Visit", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_17)
 public class VisitPladdon extends Pladdon
 {
     @Override
